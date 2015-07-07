c033898c4802ca274fdf5c9fe2fae7e74bb3ecd5
Colin Walker <cjwalker@sfu.ca>
2015-07-07 15:45:04 -0700

    clean up client code

--- lib/oauth2_rails/user.rb
+++ lib/oauth2_rails/user.rb
@@ -1,5 +1,52 @@
 require 'base'
 
 module Oauth2Rails
+  class User
 
+    def initialize(profile_body, token_body = [])
+      @token    = token_body
+      @profile  = profile_body
+    end
+
+    def profile
+      @profile
+    end
+
+    def access_token
+      @token['access_token']
+    end
+
+    def refresh_token
+      @token['refresh_token']
+    end
+
+    def id
+      @profile['user']['encodedId']
+    end
+
+    def full_name
+      @profile['user']['fullName']
+    end
+
+    def display_name
+      @profile['user']['displayName']
+    end
+
+    def country
+      @profile['user']['country']
+    end
+
+    def state
+      @profile['user']['state']
+    end
+
+    def city
+      @profile['user']['city']
+    end
+
+    def about_me
+      @profile['user']['aboutMe']
+    end
+
+  end
 end
